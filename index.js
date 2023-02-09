ae42523db036ee1ea7d3309ceb667bb003c684d7
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2023-02-09 21:48:40 +0900

    Feat(auth): user Authentication을 확인하는 auth route 추가(#13)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
 
 // application/json으로된 데이터를 가져와서 분석할수있게 하는 코드
 app.use(express.json());
@@ -23,7 +24,7 @@ app.get("/", (req, res) => {
   res.send("Hello World! 안녕~");
 });
 
-app.post("/register", (req, res) => {
+app.post("api/users/register", (req, res) => {
   // 회원 가입할때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어줌
 
@@ -68,6 +69,22 @@ app.post("/api/users/login", (req, res) => {
           .json({ loginSuccess: true, userId: user._id });
       });
     });
+  });
+});
+
+// auth 받아오기
+// 미드웨어 : request 받은 후 callback function 하기 전 중간에서 action
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과한것 => Authentication이 true라는 거!
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
