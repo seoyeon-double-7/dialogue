2f07e4ea11e2a10642334887f53b44efafaffe2a
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2023-02-13 14:09:40 +0900

    ✨ : redux, extension과 연결

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import "antd/dist/antd.css";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducer";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // redux와 연결
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      // redux extension과 연결
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
-  </React.StrictMode>
+  </Provider>
 );
