9b4b0ee11d50a9221930eb509cff6dd148ae3f3f
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2023-02-15 19:17:32 +0900

    ✨ : Logout 기능 완료

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 // import { response } from "express";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   // 랜딩 페이지 들어오자마자 실행
   // get request를 프론트로 전송
   useEffect(() => {
@@ -11,6 +14,17 @@ function LandingPage() {
       console.log(response);
     });
   }, []);
+
+  const onClickHandler = () => {
+    // 복잡하지 x 기능이므로 axios 바로 써줌
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("fail logout");
+      }
+    });
+  };
 
   return (
     <div
@@ -23,6 +37,8 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
