9b42c0b7bd0a03871cc9a1cdef648d123b3ab3c0
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2023-02-15 19:04:31 +0900

    ✨ : Register 기능 구현

--- client/src/_actions/types.js
+++ client/src/_actions/types.js
@@ -1,3 +1,4 @@
 //  타입들만 관리
 
 export const LOGIN_USER = "login_user";
+export const REGISTER_USER = "register_user";
--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { LOGIN_USER } from "./types";
+import { LOGIN_USER, REGISTER_USER } from "./types";
+
+// login action
+
 export function loginUser(dataToSubmit) {
   const request = axios
     // 백엔드 서버 url에 dataToSubmit 데이터 보내주기
@@ -9,6 +12,21 @@ export function loginUser(dataToSubmit) {
   return {
     // redux로 옮겨주기
     type: LOGIN_USER,
+    payload: request,
+  };
+}
+
+// register action
+
+export function registerUser(dataToSubmit) {
+  const request = axios
+    // 백엔드 서버 url에 dataToSubmit 데이터 보내주기
+    .post("/api/users/register", dataToSubmit)
+    .then((response) => response.data);
+
+  return {
+    // redux로 옮겨주기
+    type: REGISTER_USER,
     payload: request,
   };
 }
