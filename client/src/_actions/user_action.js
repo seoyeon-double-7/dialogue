c6f7ce48bbdb4e5a6f3ff435b17b5463d600c5aa
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2023-02-16 17:21:29 +0900

    ✨ : 로그인, 로그아웃할때 Auth체크 기능 추가

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_USER, REGISTER_USER } from "./types";
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from "./types";
 
 // login action
 
@@ -27,6 +27,18 @@ export function registerUser(dataToSubmit) {
   return {
     // redux로 옮겨주기
     type: REGISTER_USER,
+    payload: request,
+  };
+}
+export function auth() {
+  const request = axios
+    // 백엔드 서버 url에 dataToSubmit 데이터 보내주기
+    .get("/api/users/auth")
+    .then((response) => response.data);
+
+  return {
+    // redux로 옮겨주기
+    type: AUTH_USER,
     payload: request,
   };
 }
---
+++ client/src/hoc/auth.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { auth } from "../_actions/user_action";
+
+export default function f(SpecificComponent, option, adminRoute = null) {
+  // option
+  // null(아무나 출입 가능한 페이지)
+  // true(로그인한 유저만 출입가능한 페이지)
+  // false(로그인한 유저는 출입불가능한 페이지)
+
+  function AuthenticationCheck(props) {
+    // request로 백엔드에서 상태 가져오기
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      dispatch(auth()).then((response) => {
+        console.log(response);
+      });
+    }, []);
+    return <SpecificComponent />;
+  }
+
+  return AuthenticationCheck;
+}
